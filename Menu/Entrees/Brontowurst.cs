6ad41493cc4f5bd9e0e4f4b4fa196ae379263b9e
jjohn98 <jjohn98@ksu.edu>
2019-09-09 17:17:34 -0500

    first commit
    added brontowurst

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    class Brontowurst
+    {
+        private bool peppers = true;
+        private bool onions = true;
+        private bool bun = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Brautwurst" };
+                if (bun) ingredients.Add("Whole-Wheat Bun");
+                if (onions) ingredients.Add("Onions");
+                if (peppers) ingredients.Add("Peppers");
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Price = 5.36;
+            this.Calories = 498;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+
+        public void HoldOnion()
+        {
+            this.onions = false;
+        }
+    }
+}
