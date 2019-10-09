af4edc814642fe9901f0048ceccedf59b553cfa3
jjohn98 <jjohn98@ksu.edu>
2019-10-09 16:48:01 -0500

    16 or17 th commit added some neat stuff towards pos milestone 1

---
+++ PointOfSale/MenuCategorySelection.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for MenuCategorySelection.xaml
+    /// </summary>
+    public partial class MenuCategorySelection : Page
+    {
+        public MenuCategorySelection()
+        {
+            InitializeComponent();
+        }
+
+        private void Combo_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ComboPage());
+        }
+
+        private void Entrees_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EntreesPage());
+        }
+
+        private void Sides_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new SidesPage());
+        }
+
+        private void Drinks_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DrinksPage());
+        }
+    }
+}
