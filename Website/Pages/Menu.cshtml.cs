2735ae4f1ff53c56c3ac399e39a98756092caee2
jjohn98 <jjohn98@ksu.edu>
2019-11-22 16:58:39 -0600

    36th(?) commit added some more buttons and stuff thats about it.

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -10,7 +10,43 @@ namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
-        public Menu menu = new Menu();
+        public Menu menu;
+
+        public MenuModel()
+        {
+            menu = new Menu();
+            entrees = menu.AvailableEntrees;
+            sides = menu.AvailableSides;
+            drinks = menu.AvailableDrinks;
+            combos = menu.AvailableCombos;
+        }
+
+        public double minPrice = 0;
+
+        public double maxPrice = 10;
+
+        public List<IMenuItem> entrees = new List<IMenuItem>();
+
+        public List<IMenuItem> sides = new List<IMenuItem>();
+
+        public List<IMenuItem> drinks = new List<IMenuItem>();
+
+        public List<IMenuItem> combos = new List<IMenuItem>();
+
+        public static List<IMenuItem> Search(List<IMenuItem> items, string searchstring)
+        {
+            List<IMenuItem> result = new List<IMenuItem>();
+
+            foreach (IMenuItem item in items)
+            {
+                if (item.ToString().Contains(searchstring, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
 
         public void OnGet()
         {
